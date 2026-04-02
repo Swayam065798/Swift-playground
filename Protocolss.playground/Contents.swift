e04640305e38368b6fbce3b1492f823f63e48757
Swayamdipta <swayamdiptab@gmail.com>
2026-04-02 21:41:51 +0530

    Added protocols composition and extension code

--- Protocolss.playground/Contents.swift
+++ Protocolss.playground/Contents.swift
@@ -17,3 +17,19 @@ struct User: MySampleProtocol {
     var name: String
 }
  let user = User(name: "Swayam")
+ print(user.getMyname())
+
+
+// Multiple protocols --> Conformi
+protocol Flyable {
+    func fly()
+}
+
+protocol Swimmable {
+    func swim()
+}
+
+struct Duck: Flyable, Swimmable {
+    func fly() { print("Duck is flying") }
+    func swim() { print("Duck is swimming") }
+}

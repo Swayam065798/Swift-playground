d068759c4f01387e07de7bff335c1a6cf72b513f
Swayamdipta <swayamdiptab@gmail.com>
2026-04-05 00:20:47 +0530

    Added DSA problem for returning the count and character of non-repeating characters problem

--- DSA.playground/Contents.swift
+++ DSA.playground/Contents.swift
@@ -16,3 +16,36 @@ func getFrequency(arr: [Int]) -> [Int: Int] {
 let arr = [1, 2, 2, 3, 1, 4, 2]
 let result = getFrequency(arr: arr)
 print(result)
+
+// MARK: first non-repeating characters returning the character alonwith the count
+class Solution {
+    var count = 0
+    func firstNonRepeatingCharacter(_ s: String) -> (Character, Int) {
+        // Step 1: Count frequency of each character
+        var frequency: [Character: Int] = [:]
+        
+        
+        for char in s {
+            frequency[char, default: 0] += 1
+        }
+        
+        // Step 2: Find first char with count == 1
+        for char in s {
+            if frequency[char] == 1 {
+                count += 1
+                return (char, count)
+            }
+        }
+        
+        // Step 3: No unique character found
+        return ("_", count)
+    }
+}
+
+let sol = Solution()
+print(sol.firstNonRepeatingCharacter("aabbcd"))  // "c"
+print(sol.count)
+let bal = Solution()
+print(bal.firstNonRepeatingCharacter("aabb"))    // "_"
+print(bal.count)
+//print(sol.firstNonRepeatingCharacter("swift"))   // "w" wait no "s"
---
+++ Decoding/Decoding/SampleModel.swift
@@ -0,0 +1,68 @@
+//
+//  SampleModel.swift
+//  Decoding
+//
+//  Created by Nilesh on 30/01/22.
+//
+
+import Foundation
+
+// MARK: - SampleModel
+struct SampleModel: Codable {
+    let members: [Member]
+    
+    enum CodingKeys: String, CodingKey {
+        case members
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        members = try container.decodeIfPresent(String.self, forKey: <#T##KeyedDecodingContainer<CodingKeys>.Key#>)
+    }
+}
+// MARK: - Member
+struct Member: Codable {
+    let firstName, lastName: String
+    let age: MyAgeType
+    let dateCreated: Date
+    let secretIdentity: String
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName, lastName, age
+        case dateCreated = "formed"
+        case secretIdentity
+        
+    }
+  
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        firstName = try container.decode(String.self, forKey: .firstName)
+        lastName = try (container.decodeIfPresent(String.self, forKey: .lastName)) ?? "N/A"
+        dateCreated = try container.decode(Date.self, forKey: .dateCreated)
+        age = try container.decode(MyAgeType.self, forKey: .age)
+        secretIdentity = try container.decode(String.self, forKey: .secretIdentity)
+        
+    }
+}
+
+// Decoding the data when 2 datatype values are returned in an API model
+enum MyAgeType: Codable {
+    case int(Int)
+    case string(String)
+    
+    init(from decoder: Decoder) throws {
+        if let intValue = try? decoder.singleValueContainer().decode(Int.self) {
+            self = .int(intValue)
+            return
+        }
+        if let stringValue = try? decoder.singleValueContainer().decode(String.self) {
+            self = .string(stringValue)
+            return
+        }
+        throw MyAgeTypeError.missingValue
+    }
+    
+    enum MyAgeTypeError: Error {
+        case missingValue
+    }
+}
